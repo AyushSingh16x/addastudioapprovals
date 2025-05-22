26f740a783417616d98bd4d34eefba3c8be3ad7e
AyushSingh16x <169048839+AyushSingh16x@users.noreply.github.com>
2025-05-22 16:20:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 const TELEGRAM_BOT_TOKEN = '8115048642:AAGYjFR4LHgmJWjUoNZJ77o452Mr1IoSE1s';
 const TELEGRAM_CHAT_ID = '4966626762';
